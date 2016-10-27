418b9d463ac7d0c428abd0c20c24728ed2792c3c
sumant041 <sumantnidagundi525@gmail.com>
2016-10-27 17:05:04 +0530

    Adding jQuery and updating calci.js

---
+++ Grunt.js
@@ -0,0 +1,9 @@
+{
+  "name": "cacli"
+  "versin":"0.1.0"
+  "devDependencies":{
+    "grunt-contrib-jshint": "~0.10.0",
+    "grunt-contrib-nodeunit": "~0.4.1",
+    "grunt-contrib-uglify": "~0.5.0"
+  }
+}
